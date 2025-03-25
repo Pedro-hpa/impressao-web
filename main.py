55a830f2cc4d1459fa5df6f89afe7e69681a2914
pedro henrique <pedro.hq.pereira@outlook.com>
2025-03-25 15:04:07 -0300

    Atualizando HTML para abrir PDF em nova aba

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ SPRINTER_DIR = os.path.join(BASE_DIR, "static/sprinter")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/") # Listar pdf caminhao
 def listar_pdfs(request: Request):
     arquivos = [f for f in os.listdir(PDF_DIR) if f.endswith(".pdf")]
     return templates.TemplateResponse("index.html", {
@@ -22,7 +22,7 @@ def listar_pdfs(request: Request):
         "arquivos": arquivos
     })
 
-@app.get("/sprinter")
+@app.get("/sprinter") # Listar pdf sprinter
 def listar_sprinter(request: Request):
     arquivos = [f for f in os.listdir(SPRINTER_DIR) if f.endswith(".pdf")]
     return templates.TemplateResponse("index.html", {
@@ -30,12 +30,12 @@ def listar_sprinter(request: Request):
         "arquivos": arquivos
     })
 
-@app.get("/pdf/{arquivo}")
+@app.get("/pdf/{arquivo}") # Pesquisa caminhao
 def abrir_pdf(arquivo: str):
     caminho = os.path.join(PDF_DIR, arquivo)
     return FileResponse(caminho, media_type="application/pdf")
 
-@app.get("/sprinter-pdf/{arquivo}")
+@app.get("/sprinter-pdf/{arquivo}") # Pesquisa caminhao
 def abrir_pdf_sprinter(arquivo: str):
     caminho = os.path.join(SPRINTER_DIR, arquivo)
     return FileResponse(caminho, media_type="application/pdf")
